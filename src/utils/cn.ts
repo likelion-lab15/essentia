d2e826dd72d525f8433a7882690563e8e14d582e
김진우 <rlawlsdn263@naver.com>
2023-11-28 00:13:28 +0900

    [⚙] ESLint, Prettier 환경 설정
    - ESLint 설정
    - Prettier 설정
    - Babel 설정
    - eslint-config-prettier 설치
    
    ESLint, Prettier를 환경을 설정했고, 그 과정에서 발생한 바벨 파싱 에러를 해결했다.
    
    ref: #24

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,5 +1,5 @@
-import { twMerge } from 'tailwind-merge';
-import { clsx, ClassValue } from 'clsx';
+import { twMerge } from "tailwind-merge";
+import { clsx, ClassValue } from "clsx";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
