33947e40aeeb8b0d85ee01bcc65efa577e2fd37d
김진우 <rlawlsdn263@naver.com>
2023-11-29 13:41:40 +0900

    [🗼markup] Footer 컴포넌트 마크업
    Footer 컴포넌트의 기본 레이아웃을 설계했다.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,58 @@
+export default function Footer() {
+  return (
+    <footer>
+      {/* 상단 콘텐츠 */}
+      <div>
+        {/* 상단 콘텐츠 - 1번 */}
+        <div>
+          <div>
+            <p>Logo</p>
+            <p>Address:</p>
+            <p>123 Main Street, New York, NY 1001</p>
+          </div>
+          <div>
+            <span>Contact</span>
+            <span>010-2739-5166</span>
+          </div>
+          <div>
+            <span>페이스북</span>
+            <span>인스타그램</span>
+            <span>트위터</span>
+            <span>유튜브</span>
+          </div>
+        </div>
+        <div>
+          {/* 상단 콘텐츠 - 2번 */}
+          <p>고객센터</p>
+          <ul>
+            <li>FAQ</li>
+            <li>공지사항</li>
+            <li>제휴문의</li>
+            <li>입점상담</li>
+            <li>멤버십 안내</li>
+          </ul>
+        </div>
+        <div>
+          {/* 상단 콘텐츠 - 3번 */}
+          <p>회사소개</p>
+          <ul>
+            <li>ABOUT ESSENTIA</li>
+            <li>회사소개</li>
+            <li>이용약관</li>
+            <li>개인정보처리방침</li>
+            <li>ESG</li>
+          </ul>
+        </div>
+      </div>
+      {/* 하단 콘텐츠 */}
+      <div>
+        <span>2023 Perfume Trading. All rights reserved.</span>
+        <div>
+          <span>Privacy Policy</span>
+          <span>Terms of Use</span>
+          <span>Cookie Policy</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
