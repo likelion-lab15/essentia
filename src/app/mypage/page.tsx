2ab08c52634200fec6698230ab224ed2ccdcf1e6
김진우 <rlawlsdn263@naver.com>
2023-11-29 23:38:23 +0900

    [🗼markup] 필터바 마크업 수정
    필터바는 향후에 Filter 공용 컴포넌트로 만들어질 예정이다.

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -9,10 +9,28 @@ export default function MyPage() {
 
       {/* 하단 콘텐츠 */}
       <div>
-        <p>MY PAGE</p>
         <div>
-          <div>{/* 필터 */}</div>
-          <div>{/* 페이지 콘텐츠 */}</div>
+          {/* 필터 */}
+          <div>
+            <p>MY PAGE</p>
+            <div>
+              <p>쇼핑 정보</p>
+              <ul>
+                <li>전체보기</li>
+                <li>구매내역</li>
+                <li>판매내역</li>
+                <li>관심상품</li>
+              </ul>
+            </div>
+            <div>
+              <p>회원 정보</p>
+              <ul>
+                <li>회원정보 수정</li>
+              </ul>
+            </div>
+          </div>
+          {/* 페이지 콘텐츠 */}
+          <div></div>
         </div>
       </div>
     </main>
