96a7925dc02e475a2b9971d8d41780fb30ee106a
김진우 <rlawlsdn263@naver.com>
2023-11-29 11:22:05 +0900

    [✨feat] 로그인 기능 구현
    <form>을 다루고 있어 서버 액션을 사용해보려 했지만, 추가 학습이 필요한 것으로 보여 현재는 클라이언트 컴포넌트에서 작업을 했다.
    
    `/users/login`으로 email과 password가 담긴 데이터를 POST 통신을 전송해 로그인 기능을 완성했다.
    
    useRef를 사용해 input의 데이터에 접근을 했고, 테스팅 목적으로 value에 고정값을 넣어뒀다.
    
    ref: #21

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Link from "next/link";
+import { useRef } from "react";
+import axios from "axios";
+
+const SignIn = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  /* 로그인 함수 */
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    await axios
+      .post("https://localhost/api/users/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log("로그인 성공", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <main>
+      <h2>로그인</h2>
+
+      <form onSubmit={handleSignIn}>
+        <label htmlFor="email">이메일</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          ref={emailRef}
+          value="s1@market.com"
+          placeholder="example@essentia.co.kr"
+        />
+        <div aria-live="polite">아이디를 확인해주세요</div>
+        <label htmlFor="password">비밀번호</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          ref={passwordRef}
+          value="11111111"
+          placeholder="영문 대/소문자, 숫자 및 특수문자를 포함한 비밀번호"
+        />
+        <div aria-live="polite">비밀번호를 확인해주세요</div>
+        <button type="submit">이메일로 로그인</button>
+      </form>
+
+      <div>
+        <Link href="/signup">이메일 회원가입</Link>
+        <button>이메일 찾기</button>
+        <button>비밀번호 찾기</button>
+      </div>
+
+      <div>
+        <button type="button">카카오로 로그인</button>
+        <button type="button">네이버로 로그인</button>
+      </div>
+    </main>
+  );
+};
+
+export default SignIn;
