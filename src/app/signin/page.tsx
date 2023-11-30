529726666ce5461992ecfca50be05fd8f3bfbb27
김진우 <rlawlsdn263@naver.com>
2023-11-30 13:44:47 +0900

    [✨feat] 로그인 경고 메세지 기능
    사용자가 로그인에 실패했을 경우, `input` 하단에 경고 메세지를 보이도록 했다.
    
    로그인에 실패할 경우, `checkUser`가 `false`가 되고 `cn()` 유틸리티 함수가 동작해 `<div>`의
    `hidden`을 `block`으로 변경한다.
    
    ref: #38

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -26,7 +26,6 @@ export default function SignIn() {
   useEffect(() => {
     if (user._id) {
       localStorage.setItem("user", JSON.stringify(user));
-      alert("로그인 성공!");
     }
   }, [user]);
 
