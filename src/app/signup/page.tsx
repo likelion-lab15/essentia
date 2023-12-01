180947fca494377dc6c8ace321006383d0e9f027
kimzeze <fridaynight@kakao.com>
2023-12-01 13:20:46 +0900

    [🗼markup] 현재 만들어진 각 페이지에 Header 추가
    중간 발표를 위해 각 페이지에 임시 헤더를 추가했다

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { AddressModal } from "@/components/_index";
 import axios from "axios"; // Import Axios
+import Header from "@/components/Header";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
@@ -105,127 +106,130 @@ export default function SignUp() {
   };
 
   return (
-    <main className="flex flex-col items-center pt-[120px]">
-      <h2 className="mb-[66px] text-36 font-bold">회원가입</h2>
+    <div>
+      <Header />
+      <main className="flex flex-col items-center pt-[120px]">
+        <h2 className="mb-[66px] text-36 font-bold">회원가입</h2>
 
-      <form onSubmit={handleSubmit} className="flex w-[400px] flex-col">
-        {/* 이메일 주소 */}
-        <label htmlFor="email">
-          로그인에 사용할 이메일 주소를 입력해주세요
-        </label>
-        <input
-          id="email"
-          type="email"
-          aria-errormessage="emailError"
-          aria-invalid={!isEmailValid}
-          onChange={handleEmailChange}
-          placeholder="example@essentia.co.kr"
-        />
-        {/* 이메일 오류 경고창 */}
-        {showEmailError && (
-          <div id="emailError" aria-live="polite" className="text-red-500">
-            이메일 오류 메세지
+        <form onSubmit={handleSubmit} className="flex w-[400px] flex-col">
+          {/* 이메일 주소 */}
+          <label htmlFor="email">
+            로그인에 사용할 이메일 주소를 입력해주세요
+          </label>
+          <input
+            id="email"
+            type="email"
+            aria-errormessage="emailError"
+            aria-invalid={!isEmailValid}
+            onChange={handleEmailChange}
+            placeholder="example@essentia.co.kr"
+          />
+          {/* 이메일 오류 경고창 */}
+          {showEmailError && (
+            <div id="emailError" aria-live="polite" className="text-red-500">
+              이메일 오류 메세지
+            </div>
+          )}
+
+          {/* 비밀번호 */}
+          <label htmlFor="password">사용할 비밀번호를 입력해주세요</label>
+          <input
+            id="password"
+            type="password"
+            aria-errormessage="passwordError"
+            aria-invalid={!isPasswordValid}
+            onChange={handlePasswordChange}
+            placeholder="영문 대/소문자, 숫자 및 특수문자를 포함한 비밀번호"
+          />
+          <input
+            id="passwordCheck"
+            type="password"
+            aria-errormessage="passwordError"
+            placeholder="비밀번호 확인"
+          />
+          {/* 비밀번호 오류 경고창 */}
+          {showPasswordError && (
+            <div id="passwordError" aria-live="polite" className="text-red-500">
+              비밀번호 오류 메세지
+            </div>
+          )}
+
+          {/* 이름 & 휴대폰번호 */}
+          <label htmlFor="name">이름과 휴대폰 번호를 입력해주세요</label>
+          <input
+            id="name"
+            type="text"
+            aria-errormessage="nameError"
+            placeholder="이름"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div id="nameError" aria-live="polite">
+            이름 오류 메세지
           </div>
-        )}
+          <label htmlFor="phone"></label>
+          <input
+            id="phone"
+            type="tel"
+            aria-errormessage="phoneError"
+            aria-invalid={!isPhoneValid}
+            onChange={handlePhoneChange}
+            placeholder="휴대폰 번호('_')제외"
+          />
+          {/* 휴대폰번호 오류 경고창*/}
+          {showPhoneError && (
+            <div id="phoneError" aria-live="polite" className="text-red-500">
+              휴대폰 번호 오류 메세지
+            </div>
+          )}
 
-        {/* 비밀번호 */}
-        <label htmlFor="password">사용할 비밀번호를 입력해주세요</label>
-        <input
-          id="password"
-          type="password"
-          aria-errormessage="passwordError"
-          aria-invalid={!isPasswordValid}
-          onChange={handlePasswordChange}
-          placeholder="영문 대/소문자, 숫자 및 특수문자를 포함한 비밀번호"
-        />
-        <input
-          id="passwordCheck"
-          type="password"
-          aria-errormessage="passwordError"
-          placeholder="비밀번호 확인"
-        />
-        {/* 비밀번호 오류 경고창 */}
-        {showPasswordError && (
-          <div id="passwordError" aria-live="polite" className="text-red-500">
-            비밀번호 오류 메세지
+          {/* 생년월일 */}
+          <label htmlFor="birth">생년월일을 입력해주세요</label>
+          <input
+            id="birth"
+            type="date"
+            aria-errormessage="birthError"
+            placeholder="예) 19990707"
+            onChange={(e) => setBirth(e.target.value)}
+          />
+          {/* 생년월일 오류 경고창*/}
+          <div id="birthError" aria-live="polite">
+            생년월일 오류 메세지
           </div>
-        )}
 
-        {/* 이름 & 휴대폰번호 */}
-        <label htmlFor="name">이름과 휴대폰 번호를 입력해주세요</label>
-        <input
-          id="name"
-          type="text"
-          aria-errormessage="nameError"
-          placeholder="이름"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <div id="nameError" aria-live="polite">
-          이름 오류 메세지
-        </div>
-        <label htmlFor="phone"></label>
-        <input
-          id="phone"
-          type="tel"
-          aria-errormessage="phoneError"
-          aria-invalid={!isPhoneValid}
-          onChange={handlePhoneChange}
-          placeholder="휴대폰 번호('_')제외"
-        />
-        {/* 휴대폰번호 오류 경고창*/}
-        {showPhoneError && (
-          <div id="phoneError" aria-live="polite" className="text-red-500">
-            휴대폰 번호 오류 메세지
+          {/* 주소 */}
+          <label htmlFor="address">주소를 입력해주세요</label>
+          <input
+            id="address"
+            type="text"
+            aria-errormessage="addressError"
+            placeholder="도로명 주소를 입력해주세요"
+            value={address} // Display the selected address
+            readOnly // Make the field read-only
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <button type="button" onClick={() => setAddressModalOpen(true)}>
+            주소 검색
+          </button>
+          <AddressModal
+            isOpen={isAddressModalOpen}
+            onClose={() => setAddressModalOpen(false)}
+            onSelectAddress={handleAddressChange}
+          />
+          <label htmlFor="addressDetail"></label>
+          <input
+            id="addressDetail"
+            type="text"
+            aria-errormessage="addressError"
+            placeholder="상세 주소를 입력해주세요"
+          />
+          {/* 주소 오류 경고창*/}
+          <div id="addressError" aria-live="polite">
+            주소 오류 메세지
           </div>
-        )}
 
-        {/* 생년월일 */}
-        <label htmlFor="birth">생년월일을 입력해주세요</label>
-        <input
-          id="birth"
-          type="date"
-          aria-errormessage="birthError"
-          placeholder="예) 19990707"
-          onChange={(e) => setBirth(e.target.value)}
-        />
-        {/* 생년월일 오류 경고창*/}
-        <div id="birthError" aria-live="polite">
-          생년월일 오류 메세지
-        </div>
-
-        {/* 주소 */}
-        <label htmlFor="address">주소를 입력해주세요</label>
-        <input
-          id="address"
-          type="text"
-          aria-errormessage="addressError"
-          placeholder="도로명 주소를 입력해주세요"
-          value={address} // Display the selected address
-          readOnly // Make the field read-only
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <button type="button" onClick={() => setAddressModalOpen(true)}>
-          주소 검색
-        </button>
-        <AddressModal
-          isOpen={isAddressModalOpen}
-          onClose={() => setAddressModalOpen(false)}
-          onSelectAddress={handleAddressChange}
-        />
-        <label htmlFor="addressDetail"></label>
-        <input
-          id="addressDetail"
-          type="text"
-          aria-errormessage="addressError"
-          placeholder="상세 주소를 입력해주세요"
-        />
-        {/* 주소 오류 경고창*/}
-        <div id="addressError" aria-live="polite">
-          주소 오류 메세지
-        </div>
-
-        <button type="submit">회원가입 완료</button>
-      </form>
-    </main>
+          <button type="submit">회원가입 완료</button>
+        </form>
+      </main>
+    </div>
   );
 }
