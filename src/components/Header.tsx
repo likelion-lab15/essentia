0da56b190580aad3496a16080e43fcbb5ae0c1ef
kimzeze <fridaynight@kakao.com>
2023-12-04 22:29:20 +0900

    [💄style] Header 레이아웃 스타일 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,93 +1,91 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header role="banner" className="border-primary h-[80px] border">
+    <header
+      role="banner"
+      className="h-[80px] border-b border-primary pl-[60px] pr-[60px]"
+    >
       <nav
         aria-label="Main navigation"
         className="flex items-center justify-between"
       >
         <Link
           href="/"
-          className="flex h-[80px] w-[280px] items-center justify-center"
+          className="flex h-[80px] w-[140px] items-center justify-center"
         >
-          Home
+          <Image src="/Logo.png" alt="Home" width={120} height={55} />
         </Link>
-        <ul className="flex space-x-4">
+        <ul className="text-primary-500 flex w-[500px] justify-between text-16 ">
           <li>
-            <Link href="/about" className="text-primary-500" accessKey="1">
+            <Link href="/about" className="" accessKey="1">
               ABOUT
             </Link>
           </li>
           <li>
-            <Link href="/best" className="text-#222-500" accessKey="2">
+            <Link href="/best" className="text-primary-500" accessKey="2">
               BEST
             </Link>
           </li>
           <li>
-            <Link href="/brand" className="text-#222-500" accessKey="3">
+            <Link href="/brand" className="text-primary-500" accessKey="3">
               BRAND
             </Link>
           </li>
           <li>
-            <Link href="/contact" className="text-#222-500" accessKey="4">
+            <Link href="/contact" className="text-primary-500" accessKey="4">
               WOMEN
             </Link>
           </li>
           <li>
-            <Link href="/men" className="text-#222-500" accessKey="4">
+            <Link href="/men" className="text-primary-500" accessKey="4">
               MEN
             </Link>
           </li>
           <li>
-            <Link href="/magazine" className="text-#222-500" accessKey="4">
+            <Link href="/magazine" className="text-primary-500" accessKey="4">
               MAGAZINE
             </Link>
           </li>
         </ul>
 
-        <div>
-          {/* <button
+        <div className="flex h-[80px] w-[280px] justify-between pl-[80px] ">
+          <button
             aria-label="Open search"
             onClick={() => {}}
-            className=" mx-2 h-3.5 w-3.5 bg-center bg-no-repeat"
+            className="bg-center bg-no-repeat"
           >
-            <span>Search</span>
-          </button> */}
-          <Link
-            href="/signup"
-            aria-label="sign up"
+            <Image src="/search-icon.svg" alt="Home" width={24} height={24} />
+          </button>
+          <button
+            aria-label="My Page"
             onClick={() => {}}
-            className=" mx-2 h-3.5 w-3.5 bg-center bg-no-repeat"
+            className="bg-center bg-no-repeat"
           >
-            <span>Sign up</span>
-          </Link>
-          <Link
-            href="/mypage"
-            aria-label="My page"
+            <Image src="/user-icon.svg" alt="Home" width={24} height={24} />
+          </button>
+          <button
+            aria-label="Sign In"
             onClick={() => {}}
-            className=" mx-2 h-3.5 w-3.5 bg-center bg-no-repeat"
+            className="bg-center bg-no-repeat"
           >
-            <span>My Page</span>
-          </Link>
-          <Link
-            href="/signin"
-            aria-label="Sign in"
-            onClick={() => {}}
-            className=" mx-2 h-3.5 w-3.5 bg-center bg-no-repeat"
-          >
-            <span>login</span>
-          </Link>
-          <Link
-            href="/wishlist"
+            <Image src="/signin-icon.svg" alt="Home" width={24} height={24} />
+          </button>
+          <button
             aria-label="Wish List"
             onClick={() => {}}
-            className="mx-2 h-3.5 w-3.5 bg-center bg-no-repeat"
+            className="bg-center bg-no-repeat"
           >
-            <span>wishlist</span>
-          </Link>
+            <Image
+              src="/blackheart-icon.svg"
+              alt="Home"
+              width={24}
+              height={24}
+            />
+          </button>
         </div>
       </nav>
     </header>
