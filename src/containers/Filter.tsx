4caa101a9ae4146030b3d1cb16e67394bb20f19a
김진우 <rlawlsdn263@naver.com>
2023-12-04 17:39:00 +0900

    [🔨refactor] Filter 공용 컴포넌트 리팩토링
    Filter 공용 컴포넌트를 재활용할 수 있도록 일부를 FilterList 컴포넌트로 만들고, title과 children
    props를 만들었다.
    
    title에는 Filter의 제목이 들어오고, children에는 FilterList 컴포넌트가 들어온다.

--- src/containers/Filter.tsx
+++ src/containers/Filter.tsx
@@ -1,45 +1,10 @@
-export default function Filter() {
-  const list = ["전체보기", "구매내역", "판매내역", "관심상품", "리뷰"];
-
+export default function Filter({ title, children }) {
   return (
     <div className="w-[250px]">
       <p className="h-[70px] cursor-default border-b-[3px] border-solid border-[#222] text-[36px] font-bold">
-        MY PAGE
+        {title}
       </p>
-      <div>
-        <p className="flex h-[46px] cursor-default items-center px-[25px] font-semibold">
-          쇼핑 정보
-        </p>
-        <ul className="mb-[20px]">
-          {list.map((item, index) => {
-            return (
-              <li
-                key={index}
-                className="flex h-[36px] cursor-pointer items-center px-[25px] font-medium text-[#808080] hover:bg-[#A0D1EF] hover:text-[#222]"
-              >
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div>
-        <p className="flex h-[46px] cursor-default items-center px-[25px] font-semibold">
-          쇼핑 정보
-        </p>
-        <ul className="mb-[20px]">
-          {list.map((item, index) => {
-            return (
-              <li
-                key={index}
-                className="flex h-[36px] cursor-pointer items-center px-[25px] font-medium text-[#808080] hover:bg-[#A0D1EF] hover:text-[#222]"
-              >
-                {item}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {children}
     </div>
   );
 }
