f6115e324d00c3fdcddcbbd61f64117c80fdfc51
김진우 <rlawlsdn263@naver.com>
2023-12-04 14:48:35 +0900

    [💄style] Filter 공용 컴포넌트 스타일 완성

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,20 +1,43 @@
 export default function Filter() {
+  const list = ["전체보기", "구매내역", "판매내역", "관심상품", "리뷰"];
+
   return (
-    <div>
-      <p>MY PAGE</p>
+    <div className="w-[250px]">
+      <p className="h-[70px] cursor-default border-b-[3px] border-solid border-[#222] text-[36px] font-bold">
+        MY PAGE
+      </p>
       <div>
-        <p>쇼핑 정보</p>
-        <ul>
-          <li>전체보기</li>
-          <li>구매내역</li>
-          <li>판매내역</li>
-          <li>관심상품</li>
+        <p className="flex h-[46px] cursor-default items-center px-[25px] font-semibold">
+          쇼핑 정보
+        </p>
+        <ul className="mb-[20px]">
+          {list.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="flex h-[36px] cursor-pointer items-center px-[25px] font-medium text-[#808080] hover:bg-[#A0D1EF] hover:text-[#222]"
+              >
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div>
-        <p>회원 정보</p>
-        <ul>
-          <li>회원정보 수정</li>
+        <p className="flex h-[46px] cursor-default items-center px-[25px] font-semibold">
+          쇼핑 정보
+        </p>
+        <ul className="mb-[20px]">
+          {list.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="flex h-[36px] cursor-pointer items-center px-[25px] font-medium text-[#808080] hover:bg-[#A0D1EF] hover:text-[#222]"
+              >
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
