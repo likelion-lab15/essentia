28e4aef244600a6a2061ee755c029b77b58b6b30
kimzeze <fridaynight@kakao.com>
2023-12-04 14:58:56 +0900

    [🧹chore] 회원가입 페이지 코드 순서 및 주석 변경
    리팩토링 및 모듈화를 하기전 각 상태관리와 함수들의 순서를 정렬했다

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,49 @@
 "use client";
+import axios from "axios";
 import React, { useState } from "react";
-import { AddressModal } from "@/components/_index";
-import axios from "axios"; // Import Axios
 import Header from "@/components/Header";
+import { AddressModal } from "@/components/_index";
 
 export default function SignUp() {
+  type AddressData = {
+    address: string;
+  };
+
+  // Input 상태관리
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [birth, setBirth] = useState("");
-  const type = "seller";
+  const [address, setAddress] = useState("");
 
+  // 유효성 상태 관리
+  const [isEmailValid, setEmailValid] = useState(true);
+  const [isEmailUnique, setEmailUnique] = useState(true);
+  const [isPasswordValid, setPasswordValid] = useState(true);
+  const [isPasswordCheckValid, setPasswordCheckValid] = useState(true);
+  const [isNameValid, setNameValid] = useState(true);
+  const [isPhoneValid, setPhoneValid] = useState(true);
+  const [isBirthValid, setBirthValid] = useState(true);
+
+  // 오류메세지 상태 관리
+  const [showNameError, setShowNameError] = useState(false);
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [showPasswordError, setShowPasswordError] = useState(false);
+  const [showPhoneError, setShowPhoneError] = useState(false);
+  const [showPasswordCheckError, setShowPasswordCheckError] = useState(false);
+  const [showBirthError, setShowBirthError] = useState(false);
+
+  // 중복확인 했는지 유무 상태 관리
+  const [hasCheckedDuplication, setHasCheckedDuplication] = useState(false);
+
+  // 주소 API 관련 상태 관리
+  const [isAddressModalOpen, setAddressModalOpen] = useState(false);
+
+  // 회원가입 제출 통신
   const sendPostRequest = async () => {
+    const type = "seller";
     try {
       const response = await axios.post("https://localhost/api/users/", {
         email,
@@ -20,92 +51,49 @@ export default function SignUp() {
         name,
         type,
         phone,
-        // birth,
         address,
       });
       console.log(response.data);
     } catch (error) {
-      console.error("axios 에러발생", error);
+      console.error("회원가입 제출 통신 에러 발생", error);
     }
   };
-
-  // 이메일 중복확인 상태
-  const [isEmailUnique, setEmailUnique] = useState(true);
 
   // 이메일 중복확인 통신
   const checkEmailDuplication = async () => {
     try {
-      const response = await axios.get(
+      const res = await axios.get(
         `https://localhost/api/users/email?email=${email}`
       );
-      // If the request is successful, set email as unique
       setEmailUnique(true);
       setShowEmailError(false);
       setHasCheckedDuplication(true);
-      console.log("Email is unique");
+      console.log(res);
+      console.log("사용가능한 이메일입니다");
     } catch (error) {
-      if (error.response && error.response.status === 409) {
-        // Handle duplicate email case
-        setEmailUnique(false);
-        setShowEmailError(true);
-        console.log("Email is a duplicate");
+      if (error instanceof Error) {
+        if (error.response && error.response.status === 409) {
+          // Handle duplicate email case
+          setEmailUnique(false);
+          setShowEmailError(true);
+          console.log("중복된 이메일입니다.");
+        } else {
+          // Handle other errors
+          console.error("이메일 중복확인 에러 ", error.message);
+        }
       } else {
-        // Handle other errors
-        console.error("Error checking email: ", error);
+        console.error("알 수 없는 오류 발생", error);
       }
     }
   };
 
-  // 중복확인 했는지 유무 상태 관리
-  const [hasCheckedDuplication, setHasCheckedDuplication] = useState(false);
-
-  // 이메일, 비밀번호, 휴대폰번호의 유효성 상태 관리
-  const [isEmailValid, setEmailValid] = useState(true);
-  const [isPasswordValid, setPasswordValid] = useState(true);
-  const [isPhoneValid, setPhoneValid] = useState(true);
-
-  // 이메일, 비밀번호, 휴대폰번호의 오류메세지 상태 관리
-  const [showEmailError, setShowEmailError] = useState(false);
-  const [showPasswordError, setShowPasswordError] = useState(false);
-  const [showPhoneError, setShowPhoneError] = useState(false);
-
-  // 주소 API 관련 상태 관리
-  const [address, setAddress] = useState("");
-  const [isAddressModalOpen, setAddressModalOpen] = useState(false);
-
-  // 비밀번호 확인을 위한 상태 관리
-  const [passwordCheck, setPasswordCheck] = useState("");
-  const [isPasswordCheckValid, setPasswordCheckValid] = useState(true);
-  const [showPasswordCheckError, setShowPasswordCheckError] = useState(false);
-
-  // 비밀번호 확인 변경 핸들러
-  const handlePasswordCheckChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const passwordCheckValue = e.target.value;
-    setPasswordCheck(passwordCheckValue);
-
-    // 비밀번호와 비밀번호 확인이 일치하는지 검사
-    const isValid = password === passwordCheckValue;
-    setPasswordCheckValid(isValid);
-    setShowPasswordCheckError(!isValid);
-  };
-
-  // 이름 상태관리
-  const [isNameValid, setNameValid] = useState(true);
-  const [showNameError, setShowNameError] = useState(false);
-
   // 이름 유효성 검사 (한글만 허용)
-  const validateName = (name) => {
+  const validateName = (name: string) => {
     return /^[가-힣]+$/.test(name);
   };
 
-  // 생년월일 상태관리
-  const [isBirthValid, setBirthValid] = useState(true);
-  const [showBirthError, setShowBirthError] = useState(false);
-
   // 생년월일 유효성 검사 (1900 ~2023년생까지 가능)
-  const validateBirth = (birth) => {
+  const validateBirth = (birth: string) => {
     return (
       /^\d{4}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])$/.test(birth) &&
       parseInt(birth.substring(0, 4)) >= 1900 &&
@@ -113,63 +101,28 @@ export default function SignUp() {
     );
   };
 
-  const handleBirthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const birthValue = e.target.value;
-    const isValid = validateBirth(birthValue);
-    setBirthValid(isValid);
-    setShowBirthError(!isValid);
-    setBirth(birthValue);
-  };
-
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const nameValue = e.target.value;
-    const isValid = validateName(nameValue);
-    setNameValid(isValid);
-    setShowNameError(!isValid);
-    setName(nameValue);
-  };
-
-  interface AddressData {
-    address: string;
-  }
-
-  /* 이메일 유효성 검사
-  1. 올바른 이메일 형식 체크 */
+  // 이메일 유효성 검사
+  // 조건 1. 올바른 이메일 형식
   const validateEmail = (email: string) => {
     return /\S+@\S+\.\S+/.test(email);
   };
 
-  /* 비밀번호 유효성 
-  조건 1. 10글자 이상 16글자 이하 
-  조건 2. 영문자와 숫자 또는 특수문자가 포함되어야 한다
-   */
+  //  비밀번호 유효성 검사
+  // 조건 1. 10글자 이상 16글자 이하
+  // 조건 2. 영문자와 숫자 또는 특수문자가 포함되어야 한다
   const validatePassword = (password: string) => {
     const isValidLength = password.length >= 10 && password.length <= 16;
     const isValidComplexity = /^(?=.*[a-zA-Z])(?=.*[\d\W])/.test(password);
     return isValidLength && isValidComplexity;
   };
 
-  /* 휴대폰번호 유효성
-  조건 1. 010으로 시작하는 11자리 숫자여야 한다
-  */
+  // 휴대폰번호 유효성
+  // 조건 1. 010으로 시작하는 11자리 숫자
   const validatePhone = (phone: string) => {
     return /^010\d{8}$/.test(phone);
   };
 
-  /* 주소 변경 핸들러 */
-  const handleAddressChange = (data: AddressData) => {
-    setAddress(data.address);
-    setAddressModalOpen(false);
-  };
-
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const phoneValue = e.target.value;
-    const isValid = validatePhone(phoneValue);
-    setPhoneValid(isValid);
-    setShowPhoneError(!isValid);
-    setPhone(phoneValue); // Update phone state
-  };
-
+  // 핸들러 함수
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const emailValue = e.target.value;
     const isValid = validateEmail(emailValue);
@@ -187,18 +140,59 @@ export default function SignUp() {
     setPassword(passwordValue);
   };
 
+  const handlePasswordCheckChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const passwordCheckValue = e.target.value;
+    const isValid = password === passwordCheckValue;
+    setPasswordCheck(passwordCheckValue);
+    setPasswordCheckValid(isValid);
+    setShowPasswordCheckError(!isValid);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nameValue = e.target.value;
+    const isValid = validateName(nameValue);
+    setNameValid(isValid);
+    setShowNameError(!isValid);
+    setName(nameValue);
+  };
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const phoneValue = e.target.value;
+    const isValid = validatePhone(phoneValue);
+    setPhoneValid(isValid);
+    setShowPhoneError(!isValid);
+    setPhone(phoneValue);
+  };
+
+  const handleBirthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const birthValue = e.target.value;
+    const isValid = validateBirth(birthValue);
+    setBirthValid(isValid);
+    setShowBirthError(!isValid);
+    setBirth(birthValue);
+  };
+
+  const handleAddressChange = (data: AddressData) => {
+    setAddress(data.address);
+    setAddressModalOpen(false);
+  };
+
+  // 회원가입 폼 제출 함수
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     // 각 입력 필드가 비어 있는지 검사
     const isEmailEmpty = !email.trim();
     const isPasswordEmpty = !password.trim();
+    const isPasswordCheckEmpty = !passwordCheck.trim();
     const isNameEmpty = !name.trim();
     const isPhoneEmpty = !phone.trim();
     const isBirthEmpty = !birth.trim();
     const isAddressEmpty = !address.trim();
 
-    // 필요한 경우 유효성 상태와 오류 메시지 상태 업데이트
+    // 유효성 상태와 오류 메시지 상태 업데이트
     setEmailValid(!isEmailEmpty);
     setPasswordValid(!isPasswordEmpty);
     setNameValid(!isNameEmpty);
@@ -211,8 +205,9 @@ export default function SignUp() {
     setShowNameError(isNameEmpty);
     setShowPhoneError(isPhoneEmpty);
     setShowBirthError(isBirthEmpty);
+    setShowPasswordCheckError(isPasswordCheckEmpty);
 
-    // 모든 필드가 유효한 경우에만 요청 보내기
+    // 모든 인풋이 유효한 경우에만 요청 보내기
     if (
       !isEmailEmpty &&
       isEmailUnique &&
@@ -351,8 +346,8 @@ export default function SignUp() {
             type="text"
             aria-errormessage="addressError"
             placeholder="도로명 주소를 입력해주세요"
-            value={address} // Display the selected address
-            readOnly // Make the field read-only
+            value={address}
+            readOnly
             onChange={(e) => setAddress(e.target.value)}
           />
           <button type="button" onClick={() => setAddressModalOpen(true)}>
