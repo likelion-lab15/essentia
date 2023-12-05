92deebc662b9973178ccc7aa27e1b1b5807eb462
kimzeze <fridaynight@kakao.com>
2023-12-05 16:16:18 +0900

    [🗼markup] Input 공용 컴포넌트 생성
    Input 컴포넌트는 회원가입과 마이페이지에서 사용된다.
    props로 다양한 속성을 받도록하여 유연한 개발과 웹접근성을 고려했다

--- src/components/_index.ts
+++ src/components/_index.ts
@@ -2,3 +2,4 @@ export { default as AddressModal } from "./AddressModal";
 export { default as FilterList } from "./FilterList";
 export { default as Header } from "./Header";
 export { default as ProductCard } from "./ProductCard";
+export { default as InputField } from "./Input";
