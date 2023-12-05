8487e2d2f1c59a64384dad8a30f40850df598f45
김진우 <rlawlsdn263@naver.com>
2023-12-05 10:57:00 +0900

    [💄style] Filter 컴포넌트 margin-left: 20px 추가

--- src/containers/Filter.tsx
+++ src/containers/Filter.tsx
@@ -1,6 +1,6 @@
 export default function Filter({ title, children }) {
   return (
-    <div className="w-[250px]">
+    <div className="mr-[20px] w-[250px]">
       <p className="h-[70px] cursor-default border-b-[3px] border-solid border-[#222] text-[36px] font-bold">
         {title}
       </p>
