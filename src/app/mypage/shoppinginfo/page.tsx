db0dc45d762b6841bec503ed6efce918e7093a3b
김진우 <rlawlsdn263@naver.com>
2023-12-05 11:01:27 +0900

    [💄style] 마이페이지 멤버쉽 박스

--- src/app/mypage/shoppinginfo/page.tsx
+++ src/app/mypage/shoppinginfo/page.tsx
@@ -3,6 +3,21 @@ export default function ShoppingInfo() {
     <section className="w-[1000px]">
       {/* 1. 여백 공간 */}
       <div className="h-[67px]"></div>
+      {/* 2. 멤버쉽 박스 */}
+      <div className="flex h-[150px] items-center justify-between bg-[#222] text-white">
+        <div className="flex-1 py-[35px] pl-[40px]">
+          <p className="text-[18px] font-regular">Membership</p>
+          <p className="text-[45px] font-bold">Welcome</p>
+        </div>
+        <div className="w-[275px] py-[35px] pl-[40px]">
+          <p className="text-[18px] font-regular">사용가능쿠폰</p>
+          <p className="text-[45px] font-bold">1</p>
+        </div>
+        <div className="w-[275px] py-[35px] pl-[40px]">
+          <p className="text-[18px] font-regular">마일리지</p>
+          <p className="text-[45px] font-bold">924</p>
+        </div>
+      </div>
     </section>
   );
 }
