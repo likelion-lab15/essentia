5ded6412dfa629cac09e20a9064c5a23dd8a6985
김진우 <rlawlsdn263@naver.com>
2023-12-06 20:45:58 +0900

    [✨feat] 판매내역 기능 추가 (#75)
    useEffect 내부에서 GET요청이 즉시실행함수로 실행이 된다. Promise가 끝나면 `sellHistory` state에 데이터가 담긴다.
    
    `sellHistory`는 배열로 판매내역은 리스트렌더링이 된다. `sellHistory`에서 선별되는 데이터는 `price`,
     `createdAt`, `name`다.
    
    각 데이터는 판매내역의 판매 일자, 판매 가격, 판매제품 이름에 접근할 때 사용된다.
    
    `truncateName`으로 `name`이 너무 길면 `OO...`으로 표시되게끔 했다.

--- src/app/mypage/shoppinginfo/_components/SellHistoryTable.tsx
+++ src/app/mypage/shoppinginfo/_components/SellHistoryTable.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { getAccessToken } from "@/utils/_index";
+
 const SellHistoryTable = () => {
+  const [sellHistory, setSellHistory] = useState([]);
+
+  useEffect(() => {
+    // 구매정보 불러오기
+    (async () => {
+      try {
+        const response = await axios.get(
+          "https://localhost/api/seller/products",
+          {
+            headers: {
+              Authorization: `Bearer ${getAccessToken()}`,
+            },
+          }
+        );
+        const data = response.data.item;
+        setSellHistory(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
   return (
     <table className="mb-[66px] w-[100%]">
       <caption className="h-[64px] border-b-[3px] border-black text-left text-[28px] font-bold">
-        등록 내역
+        판매 내역
       </caption>
       <tbody>
         <tr className="h-[50px] border-b-[1px] border-black text-[18px] font-bold">
@@ -11,25 +38,38 @@ const SellHistoryTable = () => {
           <th className="w-[10%]">판매금액</th>
           <th className="w-[10%]">상품관리</th>
         </tr>
-        <tr className="h-[50px] border-b-[1px] border-black text-center text-[18px] font-medium">
-          <td>2023.11.26</td>
-          <td className="text-left">BYREDO 상탈 33 EDT</td>
-          <td>195,000 원</td>
-          <td className="">
-            <button
-              type="button"
-              className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+        {sellHistory.map((item, index) => {
+          const { createdAt, name, price } = item;
+
+          // 상품 이름이 너무 길 때
+          const truncateName =
+            name.length > 35 ? name.slice(0, 35) + "..." : name;
+
+          return (
+            <tr
+              key={index}
+              className="h-[50px] border-b-[1px] border-black text-center text-[18px] font-medium"
             >
-              작성
-            </button>
-            <button
-              type="button"
-              className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
-            >
-              수정
-            </button>
-          </td>
-        </tr>
+              <td className="w-[10%]">{createdAt.split(" ")[0]}</td>
+              <td className="w-[30%] text-left">{truncateName}</td>
+              <td className="w-[10%]">{price.toLocaleString("ko-KR")} 원</td>
+              <td className="w-[10%]">
+                <button
+                  type="button"
+                  className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                >
+                  작성
+                </button>
+                <button
+                  type="button"
+                  className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                >
+                  수정
+                </button>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
