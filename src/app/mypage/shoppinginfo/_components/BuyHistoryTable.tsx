9fc882fd447b36df80ddacb7294c00068a013585
김진우 <rlawlsdn263@naver.com>
2023-12-06 17:44:25 +0900

    [✨feat] 구매내역 기능 추가 (#74)
    useEffect 내부에서 GET요청이 즉시실행함수로 실행이 된다. Promise가 끝나면 buyHistory state에 데이터가 담긴다.
    
    buyHistory는 배열로 구매내역은 리스트렌더링이 된다. buyHistory에서 선별되는 데이터는 cost, createdAt, products다.
    
    각 데이터는 구매내역의 구매 일자, 구매 가격, 제품 이름에 접근할 때 사용된다.
    
    products는 결제 기록을 보관하고 있는데 복수 결제 기록일 경우에는 products의 length 속성에 접근에 `외 O개`로 표시된다.

--- src/app/mypage/shoppinginfo/_components/BuyHistoryTable.tsx
+++ src/app/mypage/shoppinginfo/_components/BuyHistoryTable.tsx
@@ -1,4 +1,29 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { getAccessToken } from "@/utils/_index";
+
 const BuyHistoryTable = () => {
+  const [buyHistory, setBuyHistory] = useState([]);
+
+  useEffect(() => {
+    // 구매정보 불러오기
+    (async () => {
+      try {
+        const response = await axios.get("https://localhost/api/orders", {
+          headers: {
+            Authorization: `Bearer ${getAccessToken()}`,
+          },
+        });
+        const data = response.data.item;
+        setBuyHistory(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
   return (
     <table className="mb-[66px] w-[100%]">
       <caption className="h-[64px] border-b-[3px] border-black text-left text-[28px] font-bold">
@@ -11,7 +36,38 @@ const BuyHistoryTable = () => {
           <th>결제금액</th>
           <th>리뷰</th>
         </tr>
-        <tr className="h-[50px] border-b-[1px] border-black text-center text-[18px] font-medium">
+        {buyHistory?.map((item, index) => {
+          const { cost, createdAt, products } = item;
+
+          // 구매 상품이 1개 이상일 때
+          const more = products.length > 1 ? `외 ${products.length}개` : "";
+
+          return (
+            <tr
+              key={index}
+              className="h-[50px] border-b-[1px] border-black text-center text-[18px] font-medium"
+            >
+              <td>{createdAt.split(" ")[0]}</td>
+              <td className="text-left">{`${products[0].name} ${more}`}</td>
+              <td>{cost.total.toLocaleString("ko-KR")} 원</td>
+              <td>
+                <button
+                  type="button"
+                  className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                >
+                  작성
+                </button>
+                <button
+                  type="button"
+                  className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                >
+                  수정
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+        {/* <tr className="h-[50px] border-b-[1px] border-black text-center text-[18px] font-medium">
           <td>2023.11.26</td>
           <td className="text-left">BYREDO 상탈 33 EDT</td>
           <td>195,000 원</td>
@@ -29,7 +85,7 @@ const BuyHistoryTable = () => {
               수정
             </button>
           </td>
-        </tr>
+        </tr> */}
       </tbody>
     </table>
   );
