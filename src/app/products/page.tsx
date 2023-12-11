98e84b193971566552b70e9bc9bebfb4184b17e5
hyunzsu <kkm5169@naver.com>
2023-12-11 16:05:28 +0900

    [✨Feat] 상품 목록 페이지 브랜드 필터별 해당 목록 가져오기 (#99)
    - FilterList 컴포넌트 import
    - Products 컴포넌트에 selectedBrand props로 전달

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,29 @@
+"use client";
 import FilterList from "@/components/FilterList";
 import Products from "./_components/Products";
 import Filter from "@/containers/Filter";
 import brandList from "@/constants/brandList";
+import { useState } from "react";
 
 export default function AllProducts() {
+  const [selectedBrand, setSelectedBrand] = useState("");
+  const handleBrandSelect = (brand) => {
+    setSelectedBrand(brand);
+  };
+
   return (
     <>
       <main className="flex flex-col items-center">
         <div className="flex h-[400px] items-center justify-center">
           <h1 className=" w-[900px] border-b-[5px] border-black text-center text-50 font-bold">
-            LE LABO
+            {selectedBrand || "전체 상품"}
           </h1>
         </div>
         <section className="flex gap-[30px]">
           <Filter title="Brand">
-            <FilterList list={brandList} />
+            <FilterList list={brandList} onBrandSelect={handleBrandSelect} />
           </Filter>
-          <Products />
+          <Products selectedBrand={selectedBrand} />
         </section>
       </main>
     </>
