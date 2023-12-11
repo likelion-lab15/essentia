a476b4827e9d25777962e7748dd53e0504b0ec14
hyunzsu <kkm5169@naver.com>
2023-12-11 09:56:47 +0900

    [🗼markup] products, sell, admin 페이지 Header 제거

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,8 @@
 import Products from "./_components/Products";
-import Header from "@/components/Header";
 
 export default function AllProducts() {
   return (
     <>
-      <Header />
       <main className="flex flex-col items-center">
         <div className="flex h-[200px] items-center justify-center">
           <h1 className="mb-11 w-[900px] border-b-[5px] border-black text-center text-50 font-bold">
