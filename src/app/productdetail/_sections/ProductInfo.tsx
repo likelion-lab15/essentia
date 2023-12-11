0566abf575cb9e61737033cad32013eeb52ace77
kimzeze <fridaynight@kakao.com>
2023-12-11 23:10:26 +0900

    [✨feat] 해당 상품에서 사이즈를 선택할 수 있도록 기능 추가 (#79)
    사용자가 드롭다운박스를 통해 해당 상품의 사이즈를 선택할 수 있도록 기능을 추가했다

--- src/app/productdetail/_sections/ProductInfo.tsx
+++ src/app/productdetail/_sections/ProductInfo.tsx
@@ -10,13 +10,26 @@ export default function ProductInfo() {
     name: "",
     price: "",
     brand: "",
-    amount: "",
-    detail: "",
+    amount: [],
+    content: "",
+    image: "",
   });
+
+  const [selectedSize, setSelectedSize] = useState("사이즈를 선택해주세요");
+  const [amountView, setAmountView] = useState(false);
+
+  const handleSizeSelection = (size) => {
+    setSelectedSize(size + "ml");
+    setAmountView(false);
+  };
+
+  const setView = (event) => {
+    setSelectedSize(event.target.value);
+  };
 
   async function getProductInfo() {
     try {
-      const _id = 3;
+      const _id = 1;
       const response = await axios.get(`https://localhost/api/products/${_id}`);
       const result = response.data;
       console.log("response.data", result.item);
@@ -25,8 +38,10 @@ export default function ProductInfo() {
         price: result.item.price,
         brand: result.item.extra.brand,
         amount: result.item.extra.amount,
-        detail: result.item.extra.detail,
+        content: result.item.content,
+        image: result.item.mainImages[0].url,
       });
+      console.log(result.item.mainImages[0].url);
       return result.item;
     } catch (error) {
       console.error("Error 🥲", error);
@@ -45,10 +60,12 @@ export default function ProductInfo() {
         {/* 향수 이미지 */}
         <div className="flex h-[560px] w-[560px] flex-col items-center justify-center">
           <Image
-            alt="santal33"
-            src="/santal33.png"
+            alt="blanche"
+            src="/blanche.webp"
+            // src={product.image}
             width={450}
             height={450}
+            className="bg-[#F4F4F4]"
           ></Image>
         </div>
         {/* 사용자 상호작용 */}
@@ -57,24 +74,53 @@ export default function ProductInfo() {
             {product.brand}
           </p>
           <p className="text-30 font-medium">{product.name}</p>
-          <p className="mb-[34px] text-16 font-medium text-tertiary">
-            50ml 100ml
-          </p>
+          <div className="flex-rowtext-16 mb-[34px] flex font-medium text-tertiary">
+            {product.amount.map((amount, index) => (
+              <p className=" pr-[10px]" key={index}>
+                {amount}ml
+              </p>
+            ))}
+          </div>
           <p className="mb-[18px] w-[560px] text-14 font-medium text-tertiary">
-            {product.detail}
+            {product.content}
           </p>
           <div className="mb-[15px] flex w-[560px] flex-row items-baseline justify-end">
             <p className="mr-[14px] text-14 font-medium">50ml</p>
             <p className="text-28 font-bold">{product.price}원</p>
           </div>
 
-          <div className="mb-[16px] h-[46px] w-[560px] border">
-            drop down box
+          {/* 드롭다운 박스 */}
+          <div className="mb-[16px] h-[46px] w-[560px]">
+            <div>
+              <button
+                onClick={() => {
+                  setAmountView(!amountView);
+                }}
+                className="h-[46px] w-[560px] border border-primary bg-white pl-[20px] text-left font-medium text-primary hover:bg-secondary"
+              >
+                {selectedSize}
+              </button>
+            </div>
+            {amountView && (
+              <ul className="relative w-[560px] border-primary">
+                {product.amount.map((amount) => (
+                  <li
+                    className="relative mt-[-1px] h-[46px] w-[560px] cursor-pointer border border-primary bg-white  text-primary hover:bg-secondary"
+                    key={amount}
+                    onClick={() => handleSizeSelection(amount)}
+                  >
+                    <p className="pl-[20px] text-left leading-[46px]">
+                      {amount}ml
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
           {/* 버튼 박스 */}
-          <div className="mb-[16px] flex w-[560px] justify-between">
+          <div className="mb-[16px] flex w-[560px]">
             <Button
-              className="h-[46px] w-[275px] border border-primary bg-white text-primary"
+              className="mr-[10px] h-[46px] w-[275px] border border-primary bg-white text-primary"
               label="바로 구매하기"
               type="button"
             ></Button>
