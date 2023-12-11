3e213f009c48b8c5a71581d9df25b8757327f53f
kimzeze <fridaynight@kakao.com>
2023-12-11 16:35:51 +0900

    [✨feat] 스크롤에 따라 Header가 고정되어 따라오도록 수정
    sticky를 사용하여 스크롤 이동시 헤더가 따라올 수 있도록 수정했다

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   return (
     <header
       role="banner"
-      className="h-[80px] border-b border-primary pl-[60px] pr-[60px]"
+      className="z-99 sticky top-0 h-[80px] border-b border-primary bg-white pl-[60px] pr-[60px]"
     >
       <nav
         aria-label="메인 네비게이션"
