348dd02d60535da1a04bf11aa7f99b8d40bd2d4c
hyunzsu <kkm5169@naver.com>
2023-12-11 13:48:58 +0900

    [🗼markup] 상품 목록 페이지 필터 컴포넌트 임포트 및 레이아웃 완성

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,23 @@
+import FilterList from "@/components/FilterList";
 import Products from "./_components/Products";
+import Filter from "@/containers/Filter";
+import brandList from "@/constants/brandList";
 
 export default function AllProducts() {
   return (
     <>
       <main className="flex flex-col items-center">
-        <div className="flex h-[200px] items-center justify-center">
-          <h1 className="mb-11 w-[900px] border-b-[5px] border-black text-center text-50 font-bold">
+        <div className="flex h-[400px] items-center justify-center">
+          <h1 className=" w-[900px] border-b-[5px] border-black text-center text-50 font-bold">
             LE LABO
           </h1>
         </div>
-        <Products />
+        <section className="flex gap-[30px]">
+          <Filter title="Brand">
+            <FilterList list={brandList} />
+          </Filter>
+          <Products />
+        </section>
       </main>
     </>
   );
---
+++ src/constants/brandList.ts
@@ -0,0 +1,28 @@
+/* 상품목록 페이지 필터 목록 */
+const brandList = {
+  title: "전체보기",
+  data: [
+    {
+      title: "LE LABO",
+      href: "/",
+    },
+    {
+      title: "구매내역",
+      href: "/mypage/history/buyhistory",
+    },
+    {
+      title: "판매내역",
+      href: "/mypage/history/sellhistory",
+    },
+    {
+      title: "관심상품",
+      href: "/mypage/history",
+    },
+    {
+      title: "리뷰",
+      href: "/mypage/history",
+    },
+  ],
+};
+
+export default brandList;
