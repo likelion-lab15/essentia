b7c88c8f9c21f03b87433f3c911058b8e09b34e6
hyunzsu <kkm5169@naver.com>
2023-12-12 14:29:01 +0900

    [🧹chore] props 함수 이름 수정

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -16,12 +16,12 @@ export default function AllProducts() {
       <main className="flex flex-col items-center">
         <div className="flex h-[400px] items-center justify-center">
           <h1 className=" w-[900px] border-b-[5px] border-black text-center text-50 font-bold">
-            {selectedBrand || "전체 상품"}
+            {selectedBrand || "ALL"}
           </h1>
         </div>
         <section className="flex gap-[30px]">
           <Filter title="Brand">
-            <FilterList list={brandList} onBrandSelect={handleBrandSelect} />
+            <FilterList list={brandList} onClick={handleBrandSelect} />
           </Filter>
           <Products selectedBrand={selectedBrand} />
         </section>
