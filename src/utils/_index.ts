7939e34bfefeb55928dae79ce1df1dcd9ba822b9
김진우 <rlawlsdn263@naver.com>
2023-12-13 01:47:43 +0900

    [🔥remove] `useTokens` 커스텀훅을 만듦으로 기존의 사용되던 함수 제거

--- src/utils/_index.ts
+++ src/utils/_index.ts
@@ -1,3 +1,1 @@
 export { default as cn } from "./cn";
-export { default as fetchRefreshToken } from "./fetchRefreshToken";
-export { default as getTokens } from "./getTokens";
--- src/utils/getTokens.ts
+++
@@ -1,8 +0,0 @@
-export default function getTokens() {
-  const { token } = JSON.parse(sessionStorage.getItem("user")).state.user;
-
-  const accessToken = token.accessToken;
-  const refreshToken = token.refreshToken;
-
-  return { accessToken, refreshToken };
-}
