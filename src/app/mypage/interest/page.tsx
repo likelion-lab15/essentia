4e2776ed61244ac540ca0d45d8ce16665f01a2d5
김진우 <rlawlsdn263@naver.com>
2023-12-15 09:59:04 +0900

    [✨feat] 장바구니 목록 불러오기 기능

--- src/app/mypage/interest/page.tsx
+++ src/app/mypage/interest/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import { axiosPrivate } from "@/api/axios";
+import { useEffect } from "react";
+import { ProductCard } from "@/components/_index";
+
 export default function Interest() {
+  useEffect(() => {
+    (async () => {
+      const res = await axiosPrivate.get("carts");
+      console.log(res);
+    })();
+  }, []);
   return <section className="w-[1000px]">Interest</section>;
 }
