4336f3c10f178de129227271f5982c35f499dd93
김진우 <rlawlsdn263@naver.com>
2023-12-16 15:27:57 +0900

    [✨feat] 찜목록 불러오기 기능 (#124)
    `/bookmarks`로 GET 통신을 보내 찜목록 데이터가 담긴 배열을 받아왔다.

--- src/app/mypage/wishlist/page.tsx
+++ src/app/mypage/wishlist/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 export default function WishList() {
+  // 찜 목록 불러오기
+  const getWishList = async () => {
+    try {
+      const res = await axiosPrivate.get("bookmarks");
+      return res.data.item;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  };
+
   return (
     <section className="flex w-[1000px] flex-wrap">
     </section>
