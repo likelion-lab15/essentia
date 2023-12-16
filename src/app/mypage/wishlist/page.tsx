b8b41ea092dadc1ef7d62c108af4164ed6b9eeaf
김진우 <rlawlsdn263@naver.com>
2023-12-16 16:41:16 +0900

    [🔨refactor] '찜목록' -> '관심상품'으로 페이지 제목 수정 (#123)

--- src/app/mypage/wishlist/page.tsx
+++ src/app/mypage/wishlist/page.tsx
@@ -45,7 +45,7 @@ export default function WishList() {
     <section className="w-[1000px]">
       {/* 1. 제목 */}
       <div className="mb-[40px] flex h-[70px] items-center border-b-[3px] border-[#222]">
-        <p className="text-[28px] font-bold">찜목록</p>
+        <p className="text-[28px] font-bold">관심상품</p>
       </div>
 
       {/* 2. 장바구니 품목 */}
