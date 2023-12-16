66d2bf898dbdb83b9686ebe37d4a9bd5eedbe589
김진우 <rlawlsdn263@naver.com>
2023-12-16 15:30:31 +0900

    [✨feat] 찜목록 데이터 바인딩하기 (#124)
    GET 통신으로 가져온 찜목록 데이터로 `wishList` state를 업데이트했다.
    
    `wishList`를 리스트 렌더링해 찜목록에 담긴 데이터들이 화면에 렌더링되도록 했다.

--- src/app/mypage/wishlist/page.tsx
+++ src/app/mypage/wishlist/page.tsx
@@ -1,5 +1,21 @@
 "use client";
+
+import { axiosPrivate } from "@/api/axios";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
 export default function WishList() {
+  const [wishList, setWishList] = useState([]);
+  const [updateFlag, setUpdateFlag] = useState(false);
+
+  // 찜 목록 불러오기
+  useEffect(() => {
+    (async () => {
+      const wishList = await getWishList();
+      setWishList(wishList);
+    })();
+  }, [updateFlag]);
+
   // 찜 목록 불러오기
   const getWishList = async () => {
     try {
@@ -14,6 +30,37 @@ export default function WishList() {
 
   return (
     <section className="flex w-[1000px] flex-wrap">
+      {wishList.map((item, index) => {
+        const { image, name, price } = item.product;
+
+        return (
+          <div
+            key={item._id}
+            className="relative mr-[16px] h-[354px] w-[234px]"
+          >
+            <Image
+              src={image.url}
+              alt={name}
+              width={243}
+              height={230}
+              className="mb-[42px] bg-gray-500"
+            />
+            <div>
+              <h3 className="ml-[17px] text-14 font-bold">Brand</h3>
+              <p className="ml-[17px] text-16 font-medium">{name}</p>
+              <p className="ml-[17px] text-14 font-semibold">
+                {price.toLocaleString()}원
+              </p>
+            </div>
+            <button
+              className="absolute right-[10px] top-[10px]"
+              onClick={() => deleteWish(item._id)}
+            >
+              <Image src="/heart_fill.svg" alt="찜" width={20} height={20} />
+            </button>
+          </div>
+        );
+      })}
     </section>
   );
 }
