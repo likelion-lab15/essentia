ffc94605b5b082c9aae8919813df35dbc1049eab
kimzeze <fridaynight@kakao.com>
2023-12-16 15:47:04 +0900

    [🔨refactor] products/[_id]/ 하위에 order, sell, buy 페이지 생성

---
+++ src/app/products/[_id]/buy/page.tsx
@@ -0,0 +1,9 @@
+export default function Buy() {
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        <h1>상품 선택 페이지</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/app/products/[_id]/sell/page.tsx
@@ -0,0 +1,9 @@
+export default function Sell() {
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        <h1>판매 페이지</h1>
+      </main>
+    </>
+  );
+}
