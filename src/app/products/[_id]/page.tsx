fb8736ad911de8d90e53e2fd683338c2e9c490e4
kimzeze <fridaynight@kakao.com>
2023-12-16 15:39:03 +0900

    [🔨refactor] products/[_id]에서 기존 product 페이지 보이도록 라우터 구조 변경 (#89)

--- src/app/products/[_id]/page.tsx
+++ src/app/products/[_id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
-import { ProductInfo, ProductDetail } from "@/app/product/_sections/_index";
+import {
+  ProductInfo,
+  ProductDetail,
+} from "@/app/products/[_id]/_sections/_index";
 
 export default function item() {
   return (
