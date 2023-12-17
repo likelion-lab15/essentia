16d58b45b909df2a2a52a57acfde02f2d223470b
김진우 <rlawlsdn263@naver.com>
2023-12-17 13:56:00 +0900

    [✨feat] 내가 쓴 리뷰 데이터 가져오기 (#122)
    `/replies`로 GET 통신을 수행해 내가 쓴 리뷰 데이터를 불러왔다.

--- src/app/mypage/reviews/page.tsx
+++ src/app/mypage/reviews/page.tsx
@@ -1,4 +1,25 @@
+"use client";
+
+import { axiosPrivate } from "@/api/axios";
+import { useState, useEffect } from "react";
+
 export default function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
+  // 리뷰 데이터 불러오기
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axiosPrivate.get("replies");
+        setReviews(res.data.item);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <section className="w-[1000px]">
       <div className="pt-[6px]">
@@ -12,6 +33,40 @@ export default function Reviews() {
               <th className="w-[30%]">리뷰 제목</th>
               <th className="w-[10%]">리뷰 관리</th>
             </tr>
+            {reviews.map((item, index) => {
+              const { createdAt, name, extra } = item;
+
+              // 제목 이름이 너무 길 때
+              const truncateName =
+                extra.title.length > 35
+                  ? name.slice(0, 35) + "..."
+                  : extra.title;
+
+              return (
+                <tr
+                  key={index}
+                  className="h-[50px] cursor-pointer border-b-[1px] border-black text-center text-[18px] font-medium"
+                  onClick={() => console.log("클릭함!")}
+                >
+                  <td className="w-[10%]">{createdAt.split(" ")[0]}</td>
+                  <td className="w-[30%] text-left">{truncateName}</td>
+                  <td className="w-[10%]">
+                    <button
+                      type="button"
+                      className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                    >
+                      삭제
+                    </button>
+                    <button
+                      type="button"
+                      className="h-[50px] w-[70px] hover:bg-[#A0D1EF]"
+                    >
+                      수정
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
