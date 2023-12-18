351573c886537f724725ab2739de409ba3a0c1ba
hyunzsu <kkm5169@naver.com>
2023-12-18 11:35:29 +0900

    [💄style] 드롭다운 메뉴 스타일링

--- src/app/products/_components/Products.tsx
+++ src/app/products/_components/Products.tsx
@@ -16,6 +16,23 @@ const getProducts = async () => {
 export default function Products({ selectedBrand }) {
   // 향수 목록 상태 관리
   const [products, setProducts] = useState([]);
+  // 드롭 다운 클릭 시
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sortOrder, setSortOrder] = useState("price-desc");
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  const handleSortSelection = (order) => {
+    setSortOrder(order);
+    setDropdownOpen(false); // 드롭다운 메뉴 닫기
+
+    const sortedProducts = [...products].sort((a, b) => {
+      return order === "price-desc" ? b.price - a.price : a.price - b.price;
+    });
+
+    setProducts(sortedProducts);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,18 +72,30 @@ export default function Products({ selectedBrand }) {
         {/* 상품 개수 */}
         <p className="mb-[110px]">{products.length}개의 상품이 있습니다.</p>
         {/* 드롭다운 박스 */}
-        <select
-          onChange={handleSortChange}
-          className="mb-[110px] border-[1px] border-primary px-[15px] py-[10px]"
-        >
-          <option
-            value="price-desc"
-            className="flex items-center justify-center"
+        <div className="mb-[110px]">
+          <button
+            onClick={toggleDropdown}
+            className="h-[40px] w-[100px] border-[1px] border-primary pr-[42px] hover:bg-secondary"
           >
-            높은 가격순
-          </option>
-          <option value="price-asc">낮은 가격순</option>
-        </select>
+            정렬
+          </button>
+          {dropdownOpen && (
+            <ul className="absolute flex h-[80px] w-[100px] flex-col gap-[10px] border-[1px] border-primary">
+              <li
+                onClick={() => handleSortSelection("price-desc")}
+                className="flex cursor-pointer items-center justify-center py-[5px] hover:bg-secondary"
+              >
+                높은 가격순
+              </li>
+              <li
+                onClick={() => handleSortSelection("price-asc")}
+                className="flex cursor-pointer items-center justify-center py-[5px] hover:bg-secondary"
+              >
+                낮은 가격순
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
       {/* 상품 목록 */}
       <ul className="flex w-[1000px] flex-row flex-wrap">
