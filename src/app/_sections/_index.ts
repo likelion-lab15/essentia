0cf920e5ad4b5a534b563fce9caf89f8363bcd2c
김진우 <rlawlsdn263@naver.com>
2023-12-21 22:43:25 +0900

    [🗼markup] `Home` 페이지 컴포넌트 기획전 캐러셀 파트 마크업

---
+++ src/app/_sections/_index.ts
@@ -0,0 +1,1 @@
+export { default as PerfumeCarousel } from "./PerfumeCarousel";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import { PerfumeCarousel } from "./_sections/_index";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <section className="flex h-[800px] w-[1280px] items-center justify-center border-2">
         <h1>메인 상단 배너</h1>
       </section>
-      <section className="flex h-[800px] w-[1280px] items-center justify-center border-2">
-        <h1>향수 기획전 캐러셀</h1>
-      </section>
+      <PerfumeCarousel />
       <section className="flex h-[800px] w-[1280px] items-center justify-center border-2">
         <h1>매거진 추천</h1>
       </section>
