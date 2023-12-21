bc7618a0c2d4761a19ec079726ee80ce22126f34
kimzeze <fridaynight@kakao.com>
2023-12-22 03:33:46 +0900

    [💄style] 메인페이지 heroSection 스타일 1차 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import HeroSection from "./_sections/HeroSection";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
-      <section className="flex h-[800px] w-[1280px] items-center justify-center border-2">
-        <h1>메인 상단 배너</h1>
-      </section>
+      <HeroSection />
       <section className="flex h-[800px] w-[1280px] items-center justify-center border-2">
         <h1>향수 기획전 캐러셀</h1>
       </section>
