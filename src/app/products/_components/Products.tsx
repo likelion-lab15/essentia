8c48cbeecfc8ca277aac21a06b32f5831195d35e
hyunzsu <kkm5169@naver.com>
2023-12-21 16:24:43 +0900

    [🔰type] 상품 목록 페이지 타입스크립트 리팩토링

--- src/app/products/_components/Products.tsx
+++ src/app/products/_components/Products.tsx
@@ -4,13 +4,26 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
+type TSortOption = {
+  key: string;
+  name: string;
+};
+
+type TProductsProps = {
+  selectedBrand: string;
+};
+
 // API 호출 함수 (페이지네이션, 정렬)
-const getProducts = async (sortOrder, page, limit) => {
+const getProducts = async (
+  sortOrder: Record<string, number> | null,
+  page: number,
+  limit: number
+) => {
   try {
     const params = new URLSearchParams({
       ...(sortOrder && { sort: JSON.stringify(sortOrder) }), // 정렬 순서가 있으면 파라미터에 추가
-      page, // 페이지 번호
-      limit, // 페이지 항목 수
+      page: page.toString(), // 페이지 번호
+      limit: limit.toString(), // 페이지 항목 수
     });
     const response = await axios.get(
       `https://localhost/api/products?${params}`
@@ -22,25 +35,28 @@ const getProducts = async (sortOrder, page, limit) => {
   }
 };
 
-export default function Products({ selectedBrand }) {
+export default function Products({ selectedBrand }: TProductsProps) {
   const PAGES_PER_GROUP = 5; // 한 페이지 그룹에 표시할 페이지 수
+  const ITEMS_PER_PAGE = 3; // 페이지 상품 수 (임시로 상품 3개)
+  const TOTAL_PAGES = 10; // 총 페이지 수 (고정 값)
+
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
-  const [itemsPerPage, setItemsPerPage] = useState(3); // 페이지 항목 수 상태 (임시로 상품 3개)
-  const [totalPages, setTotalPages] = useState(10); // 총 페이지 수 (고정 값)
   const [pageGroupStart, setPageGroupStart] = useState(1); // 페이지 그룹의 시작 페이지 번호 상태
 
   const [products, setProducts] = useState([]); // 상품 목록 상태
   const [dropdownOpen, setDropdownOpen] = useState(false); // 드롭다운 메뉴 표시 상태
   const [selectedName, setSelectedName] = useState("정렬"); // 드롭다운 메뉴 제목 상태
   // 정렬 옵션 배열
-  const sortOptions = [
+  const sortOptions: TSortOption[] = [
     { key: "price-asc", name: "낮은 가격순" },
     { key: "price-desc", name: "높은 가격순" },
   ];
 
   // '다음' 그룹으로 이동
   const handleNextGroup = () => {
-    setPageGroupStart((start) => Math.min(start + PAGES_PER_GROUP, totalPages));
+    setPageGroupStart((start) =>
+      Math.min(start + PAGES_PER_GROUP, TOTAL_PAGES)
+    );
   };
 
   // '이전' 그룹으로 이동
@@ -49,12 +65,12 @@ export default function Products({ selectedBrand }) {
   };
 
   // 페이지 변경 핸들러
-  const changePage = (newPage) => {
+  const changePage = (newPage: number) => {
     setCurrentPage(newPage);
   };
 
   // 드롭다운 메뉴에서 정렬 선택 시 호출되는 함수
-  const handleSortSelection = async (key) => {
+  const handleSortSelection = async (key: string) => {
     let sortOrder;
     if (key === "price-desc") {
       sortOrder = { price: -1 };
@@ -66,33 +82,37 @@ export default function Products({ selectedBrand }) {
     const sortedProducts = await getProducts(
       sortOrder,
       currentPage,
-      itemsPerPage
+      ITEMS_PER_PAGE
     );
     // 선택된 브랜드에 따라 상품 목록 필터링
     const filteredProducts = selectedBrand
       ? sortedProducts.filter(
-          (product) => product.extra.brand === selectedBrand
+          (product: any) => product.extra.brand === selectedBrand
         )
       : sortedProducts;
 
     setProducts(filteredProducts); // 상품 목록 상태 업데이트
-    setSelectedName(sortOptions.find((option) => option.key === key).name); // 드롭다운 메뉴 이름 업데이트
+    setSelectedName(
+      sortOptions.find((option) => option.key === key)?.name || ""
+    ); // 드롭다운 메뉴 이름 업데이트
     setDropdownOpen(false); // 드롭다운 닫기
   };
 
   // 컴포넌트가 마운트될 때 상품 데이터를 가져오는 useEffect
   useEffect(() => {
     const fetchData = async () => {
-      const allProducts = await getProducts(null, currentPage, itemsPerPage); // 기본 정렬 사용 (ID 기준)
+      const allProducts = await getProducts(null, currentPage, ITEMS_PER_PAGE); // 기본 정렬 사용 (ID 기준)
 
       const filteredProducts = selectedBrand
-        ? allProducts.filter((product) => product.extra.brand === selectedBrand)
+        ? allProducts.filter(
+            (product: any) => product.extra.brand === selectedBrand
+          )
         : allProducts;
       setProducts(filteredProducts);
     };
 
     fetchData();
-  }, [selectedBrand, currentPage, itemsPerPage]);
+  }, [selectedBrand, currentPage, ITEMS_PER_PAGE]);
 
   return (
     <div className="w-[984px]">
@@ -126,7 +146,7 @@ export default function Products({ selectedBrand }) {
       </div>
       {/* 상품 목록 */}
       <ul className="flex w-[1000px] flex-row flex-wrap">
-        {products.map((product) => (
+        {products.map((product: any) => (
           <ProductCard key={product._id} product={product} />
         ))}
       </ul>
@@ -152,7 +172,7 @@ export default function Products({ selectedBrand }) {
               className={`page-button ${
                 currentPage === page ? "text-primary" : "text-pagenation"
               } mx-[10px] px-[5px] text-45`}
-              disabled={page > totalPages}
+              disabled={page > TOTAL_PAGES}
             >
               {page}
             </button>
@@ -161,7 +181,7 @@ export default function Products({ selectedBrand }) {
         {/* 다음 버튼 */}
         <button
           onClick={handleNextGroup}
-          disabled={pageGroupStart + PAGES_PER_GROUP > totalPages}
+          disabled={pageGroupStart + PAGES_PER_GROUP > TOTAL_PAGES}
           className="ml-[20px]"
         >
           <Image src="/right-icon.png" alt="다음 버튼" width={24} height={24} />
