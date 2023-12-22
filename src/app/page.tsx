990b19cc75bb6308bebd65ca10241bb3d72a7824
kimzeze <fridaynight@kakao.com>
2023-12-22 13:33:17 +0900

    [🗼markup] `Home` 페이지 section 코드 위치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,8 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HeroSection />
       <PerfumeCarousel />
-      <section className="flex h-[800px] w-[1440px] items-center justify-center border-2">
-        <MagazineSection />
-      </section>
-      <section className="flex h-[800px] w-[1280px] items-center justify-center">
-        <BrandListSection />
-      </section>
+      <MagazineSection />
+      <BrandListSection />
     </main>
   );
 }
