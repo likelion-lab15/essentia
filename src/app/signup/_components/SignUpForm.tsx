236bfac97c24710befc7a831b873998fef7d1096
kimzeze <fridaynight@kakao.com>
2024-01-10 14:37:12 +0900

    [🔨refactor]: 회원가입(비밀번호)의 정규식을 통해 유효성 검사
    useReducer를 사용하여 비밀번호 유효성검사를 통해 에러메세지를 보여주도록 했다

--- src/app/signup/_components/SignUpForm.tsx
+++ src/app/signup/_components/SignUpForm.tsx
@@ -7,26 +7,40 @@ import { useReducer } from "react";
 type TFormState = {
   email: string;
   isEmailValid: boolean;
-  errorMessage: string;
+  password: string;
+  isPassWordValid: boolean;
+  errorMessages: {
+    email: string | null;
+    password: string | null;
+  };
 };
 
 type TFormAction =
   | { type: "UPDATE_EMAIL"; payload: string }
-  | { type: "VALIDATE_EMAIL" };
+  | { type: "UPDATE_PASSWORD"; payload: string }
+  | { type: "VALIDATE_EMAIL" }
+  | { type: "VALIDATE_PASSWORD" };
 
 const initialFormState = {
   email: "",
   isEmailValid: true,
-  errorMessage: "",
+  password: "",
+  isPassWordValid: true,
+  errorMessages: {
+    email: null,
+    password: null,
+  },
 };
 
 function formReducer(state: TFormState, action: TFormAction) {
   switch (action.type) {
+    /* 이메일 상태 업데이트 */
     case "UPDATE_EMAIL":
       return {
         ...state,
         email: action.payload,
       };
+    /* 이메일 유효성 검사 */
     case "VALIDATE_EMAIL": {
       const email = state.email.trim();
       const isValid =
@@ -36,8 +50,30 @@ function formReducer(state: TFormState, action: TFormAction) {
         );
       return {
         ...state,
-        isEmailValid: isValid,
-        errorMessage: isValid ? "" : "이메일 형식을 확인해주세요",
+        errorMessages: {
+          ...state.errorMessages,
+          email: isValid ? null : "이메일 형식을 확인해주세요",
+        },
+      };
+    }
+    /* 비밀번호 상태 업데이트 */
+    case "UPDATE_PASSWORD":
+      return {
+        ...state,
+        password: action.payload,
+      };
+    /* 비밀번호 유효성 검사 */
+    case "VALIDATE_PASSWORD": {
+      const password = state.password.trim();
+      const isValidLength = password.length >= 8 && password.length <= 16;
+      const isValidComplexity = /^(?=.*[a-zA-Z])(?=.*[\d\W])/.test(password);
+      const isValid = password === "" || (isValidLength && isValidComplexity);
+      return {
+        ...state,
+        errorMessages: {
+          ...state.errorMessages,
+          password: isValid ? null : "비밀번호 양식을 확인해주세요",
+        },
       };
     }
     default:
@@ -49,8 +85,14 @@ export default function SignUpForm() {
   const [state, dispatch] = useReducer(formReducer, initialFormState);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(state.errorMessages.email);
     dispatch({ type: "UPDATE_EMAIL", payload: e.target.value });
     dispatch({ type: "VALIDATE_EMAIL" });
+  };
+
+  const handlePassWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: "UPDATE_PASSWORD", payload: e.target.value });
+    dispatch({ type: "VALIDATE_PASSWORD" });
   };
 
   // const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -80,13 +122,39 @@ export default function SignUpForm() {
           value={state.email}
           onChange={handleEmailChange}
         />
-        {state.errorMessage && (
+        {state.errorMessages.email && (
           <div
             id={"email" + "Error"}
             aria-live="polite"
             className="flex h-[30px] w-full items-center text-warning"
           >
-            {state.errorMessage}
+            {state.errorMessages.email}
+          </div>
+        )}
+      </div>
+      {/* 비밀번호 */}
+      <div className="flex h-[106px] w-[400px] flex-col text-14">
+        <label
+          className="flex h-[38px] w-full items-center font-semibold"
+          htmlFor="password"
+        >
+          사용할 비밀번호를 입력해주세요
+        </label>
+        <input
+          className="h-[38px] w-full border-b border-primary"
+          id="password"
+          type="password"
+          placeholder="8~16 글자의 영문, 숫자, 특수문자 조합"
+          value={state.password}
+          onChange={handlePassWordChange}
+        />
+        {state.errorMessages && (
+          <div
+            id={"password" + "Error"}
+            aria-live="polite"
+            className="flex h-[30px] w-full items-center text-warning"
+          >
+            {state.errorMessages.password}
           </div>
         )}
       </div>
