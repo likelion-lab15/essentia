2088961da427df73bc5ff7cd35100b9b09b6ba4e
rlawlsdn263 <rlawlsdn263@naver.com>
2024-02-07 15:48:02 +0900

    [✨feat] `CredentialProvider` 설정
    `CredentialProvider`를 불러와 적용했다.`

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,21 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Email", type: "text", placeholder: "이메일" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "비밀번호",
+        },
+      },
+      async authorize(credentials) {},
+    }),
+  ],
 };
 
 export const handler = NextAuth(authOptions);
