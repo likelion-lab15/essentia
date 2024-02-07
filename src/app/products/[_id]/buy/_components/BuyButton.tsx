d25670c3d9804b8bfb1d9620857261976d9afb67
kimzeze <fridaynight@kakao.com>
2024-02-07 16:16:48 +0900

    [🔨refactor] 구매 선택 페이지에서 버튼을 클라이언트 컴포넌트로 분리

---
+++ src/app/products/[_id]/buy/_components/BuyButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
+
+export default function BuyButton({
+  productId,
+  buyProductId,
+}: {
+  productId: string;
+  buyProductId: number;
+}) {
+  const router = useRouter();
+  return (
+    <Button
+      className="mb-[15px] h-[60px] w-[120px] border-2 border-primary bg-secondary text-primary hover:bg-primary hover:text-secondary"
+      label="구매하기"
+      type="button"
+      onClick={() =>
+        router.push(
+          `/products/${productId}/order?&perchaseItem=${buyProductId}`
+        )
+      }
+    />
+  );
+}
