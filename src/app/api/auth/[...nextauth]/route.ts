0b1b54ef7525953e9e4d3b9b3fc2e4aa6743d9a7
rlawlsdn263 <rlawlsdn263@naver.com>
2024-02-07 15:45:53 +0900

    [✨feat] `NextAuth`를 위한 라우트 파일 설정

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+
+export const authOptions = {
+  providers: [],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
