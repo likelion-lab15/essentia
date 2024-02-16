ddd879a50b856d3eef8c69dcd89ddc9a85f07b19
rlawlsdn263 <rlawlsdn263@naver.com>
2024-02-16 15:35:25 +0900

    [✨feat] `Loader` 컴포넌트 생성
    데이터를 불러올 동안에 보여질 컴포넌트를 만들었다.

--- src/app/mypage/mywishlist/_components/_index.ts
+++ src/app/mypage/mywishlist/_components/_index.ts
@@ -1,3 +1,4 @@
 export { default as EmptyMessage } from "./EmptyMessage";
+export { default as Loader } from "./Loader";
 export { default as WishCard } from "./WishCard";
 export { default as WishCardList } from "./WishCardList";
