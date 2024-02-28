ddbbb4aa9dd29c2f98ed292d0f879a4fed26235d
kimzeze <fridaynight@kakao.com>
2024-02-28 16:11:44 +0900

    [🧹chore] Header에 영역별 주석 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,9 +17,9 @@ export default async function Header() {
         aria-label="메인 헤더 네비게이션"
         className="flex items-center justify-between pl-[60px] pr-[60px]"
       >
-        {/* 로고 이미지 */}
+        {/* 좌측 로고 이미지 */}
         <Logo />
-        {/* 페이지 네비게이션 */}
+        {/* 중앙 페이지 네비게이션 */}
         <ul className="text-primary-500 flex h-[80px] w-[600px] justify-between text-16">
           {naviList.map((naviList, index) => (
             <li className="flex items-center" key={naviList.href}>
@@ -33,8 +33,9 @@ export default async function Header() {
             </li>
           ))}
         </ul>
-        {/* 검색 & 사용자 버튼 */}
+        {/* 우측 버튼 박스 */}
         <div className="ml-[100px] mr-[-15px] flex h-[80px] w-[220px] justify-end">
+          {/* 검색 & 사용자 버튼 */}
           <ToolTip text="SEARCH">
             <button
               aria-label="검색창 열기"
@@ -48,6 +49,7 @@ export default async function Header() {
               />
             </button>
           </ToolTip>
+          {/* 마이페이지 <-> 회원가입 버튼 */}
           <ToolTip text={user ? "MYPAGE" : "SIGNUP"}>
             <Link
               href={user ? "/mypage/history" : "/signup"}
@@ -62,7 +64,9 @@ export default async function Header() {
               />
             </Link>
           </ToolTip>
+          {/* 로그인 <-> 로그아웃 버튼 */}
           <SignInToggleButton serverUserSession={user} />
+          {/* 찜 목록 보기 버튼 */}
           {user && (
             <ToolTip text="WISHLIST">
               <Link
