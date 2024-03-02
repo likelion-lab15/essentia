00616cadb9220a3f9ad4f8a8eaf73fe5ed581e24
kimzeze <fridaynight@kakao.com>
2024-03-03 00:36:58 +0900

    [🔨refactor] NEXT_PUBLIC_API_SERVER 환경 변수 통일로 인한 엔드포인트 수정

--- src/app/products/_lib/getSelectedBrand.ts
+++ src/app/products/_lib/getSelectedBrand.ts
@@ -6,7 +6,7 @@ export async function getSelectedBrand(selectedBrand: string) {
     })
   );
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER}products?custom=${customFilter}`
+    `${process.env.NEXT_PUBLIC_API_SERVER}/products?custom=${customFilter}`
   );
   if (!res.ok) {
     throw new Error("Failed to 상품 데이터 fetch");
