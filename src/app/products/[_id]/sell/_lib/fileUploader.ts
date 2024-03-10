d75bbf80f892ec5a1141e5a6e244204905641ab7
hyunzsu <hyunzsu@naver.com>
2024-03-11 02:56:26 +0900

    [🔨refactor] `fileUploader.ts` `fetch` 공용함수로 변경

--- src/app/products/[_id]/sell/_lib/fileUploader.ts
+++ src/app/products/[_id]/sell/_lib/fileUploader.ts
@@ -1,20 +1,22 @@
-/* 파일 업로드 처리 함수 */
-export const uploadFiles = async (files: any) => {
+import { fetchData } from "@/fetch/fetch";
+
+export const uploadFiles = async (files: FileList | null) => {
   const formData = new FormData();
-  files.forEach((file: any) => formData.append("attach", file)); // 파일들을 FormData에 추가
+  if (files) {
+    Array.from(files).forEach((file) => formData.append("attach", file)); // 파일들을 FormData에 추가
+  }
+
+  const options = {
+    method: "POST",
+    body: formData,
+  };
 
   try {
-    // 파일을 서버로 전송하고, 업로드된 파일의 경로를 반환 받음
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER}/files`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-
-    const responseData = await response.json();
-    return responseData.files.map((file: any) => `${file.path}`); // 서버 응답에서 파일 경로를 추출하고, 배열로 반환
+    const responseData = await fetchData("files", options);
+    if (responseData && responseData.files) {
+      return responseData.files.map((file: any) => `${file.path}`);
+    }
+    return [];
   } catch (error) {
     console.error("파일 업로드 오류", error);
     return [];
