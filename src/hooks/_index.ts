f482c070df853b3f1dcd15bf701a17cfcfa606a6
kimzeze <fridaynight@kakao.com>
2024-03-10 23:10:33 +0900

    [🔥remove] useTokens 파일 삭제

--- src/hooks/_index.ts
+++ src/hooks/_index.ts
@@ -1,5 +1,4 @@
 export { default as useOutsideClick } from "./useOutsideClick";
-export { default as useTokens } from "./useTokens";
 export { default as useModal } from "./useModal";
 export { default as useFormReducer } from "./useFormReducer";
 export { default as useClientSession } from "./useClientSession";
