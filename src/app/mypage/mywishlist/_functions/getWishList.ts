08ce06a746764418f385122eeed10adc0cf7b119
rlawlsdn263 <rlawlsdn263@naver.com>
2024-03-11 11:20:24 +0900

    [🔨refactor] `getWishList`에 fetch 공용 함수 적용

--- src/app/mypage/mywishlist/_functions/getWishList.ts
+++ src/app/mypage/mywishlist/_functions/getWishList.ts
@@ -1,18 +1,7 @@
+import { fetchPrivateData } from "@/fetch/fetch";
+
 export default async function getWishList(accessToken: string) {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}/bookmarks`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+  const wishListData = await fetchPrivateData("/bookmarks", accessToken);
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-    return data.item;
-  } catch (error) {
-    console.error("message:", error);
-  }
+  return wishListData;
 }
